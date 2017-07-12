c02a0ed1c5d6ac6dedec6a3d5c4315c8b71e53b5
Josh Barber <j.barber501@gmail.com>
2017-07-12 16:53:17 -0400

    Connect and query RFC_READ_TABLE
    Original courtesy Alex Baker http://www.alexbaker.me/code/python-and-sap-part-1-connecting-to-sap

---
+++ query-builder.py
@@ -0,0 +1,78 @@
+from pyrfc import Connection
+import re
+
+class main():
+    def __init__(self):
+        ASHOST='my.server.name'
+        CLIENT='000'
+        SYSNR='00'
+        USER='mouser'
+        PASSWD='mypassword'
+        self.conn = Connection(ashost=ASHOST, sysnr=SYSNR, client=CLIENT, user=USER, passwd=PASSWD)
+
+
+    def qry(self, Fields, SQLTable, Where = '', MaxRows=50, FromRow=0):
+        """A function to query SAP with RFC_READ_TABLE"""
+
+        # By default, if you send a blank value for fields, you get all of them
+        # Therefore, we add a select all option, to better mimic SQL.
+        if Fields[0] == '*':
+            Fields = ''
+        else:
+            Fields = [{'FIELDNAME':x} for x in Fields] # Notice the format
+
+        # the WHERE part of the query is called "options"
+        options = [{'TEXT': x} for x in Where] # again, notice the format
+
+        # we set a maximum number of rows to return, because it's easy to do and
+        # greatly speeds up testing queries.
+        rowcount = MaxRows
+
+        # Here is the call to SAP's RFC_READ_TABLE
+        tables = self.conn.call("RFC_READ_TABLE", QUERY_TABLE=SQLTable, DELIMITER='|', FIELDS = Fields, \
+                                OPTIONS=options, ROWCOUNT = MaxRows, ROWSKIPS=FromRow)
+
+        # We split out fields and fields_name to hold the data and the column names
+        fields = []
+        fields_name = []
+
+        data_fields = tables["DATA"] # pull the data part of the result set
+        data_names = tables["FIELDS"] # pull the field name part of the result set
+
+        headers = [x['FIELDNAME'] for x in data_names] # headers extraction
+        long_fields = len(data_fields) # data extraction
+        long_names = len(data_names) # full headers extraction if you want it
+
+        # now parse the data fields into a list
+        for line in range(0, long_fields):
+            fields.append(data_fields[line]["WA"].strip())
+
+        # for each line, split the list by the '|' separator
+        fields = [x.strip().split('|') for x in fields ]
+
+        # return the 2D list and the headers
+        return fields, headers
+
+
+
+# Init the class and connect
+# I find this can be very slow to do...
+s = main()
+
+# Choose your fields and table
+fields = ['MATNR', 'EAN11']
+table = 'MEAN'
+# you need to put a where condition in there... could be anything
+where = 'MATNR <> 0'
+
+# max number of rows to return
+maxrows = 10
+
+# starting row to return
+fromrow = 0
+
+# query SAP
+results, headers = s.qry(fields, table, where, maxrows, fromrow)
+
+print headers
+print results
